93e79fee779d078ae1b129b0e69f74f2bbb77dcf
oregami <oregami@163.com>
2017-03-06 17:02:57 +0800

    ip script

--- shell/ipchange.sh
+++ shell/ipchange.sh
@@ -1,21 +1,13 @@
 #!/bin/sh
 
-cd /jffs/scripts
-mkdir -p .ip/
-touch .ip/ip.txt
-touch .ip/1.log
-touch .ip/email.txt
-
-IPADDRESS=$(/sbin/ifconfig ppp0 | sed -n 's/.*inet addr:\([^ ]*\).*/\1/p')
-OLDIP=`cat .ip/ip.txt`
-
-if [ $IPADDRESS == $OLDIP ]
+touch ip.txt
+BEFORE=`cat ip.txt`
+IPADDRESS=`curl -s 1212.ip138.com/ic.asp |  iconv -f gb2312 -t utf-8 |  grep -oe '\[\(.*\)\]' | sed 's/\[//g' | sed 's/\]//g'`
+if [ "$IPADDRESS" == "$BEFORE" ]
 then
   echo "`date -u` not changed"
 else
-  echo $IPADDRESS > .ip/ip.txt
-  echo "$OLDIP=>$IPADDRESS" >> .ip/email.txt
-  /usr/sbin/sendmail -S"smtp.126.com" -f"XXX@126.com" XXX@126.com -au"XXX" -ap"PPP" < .ip/email.txt
-  echo "`date -u` changed" >> .ip/1.log
-  echo "Send email successfully!"
+  echo $IPADDRESS > ip.txt
+  curl -s http://semidream.com:10001/?text=${IPADDRESS}&to=oregami@163.com > /dev/null 2>&1
 fi
+
