a938cc4192573710a879b8e42ef6fb88b94c5b36
oregami <oregami@163.com>
2017-03-09 18:14:28 +0800

    rm comment

--- shell/ipchange-git.sh
+++ shell/ipchange-git.sh
@@ -12,11 +12,11 @@ BEFORE=`cat ip.html`
 IPADDRESS=`curl -s 1212.ip138.com/ic.asp |  iconv -f gb2312 -t utf-8 |  grep -oe '\[\(.*\)\]' | sed 's/\[//g' | sed 's/\]//g'`
 if [ "$IPADDRESS" == "$BEFORE" ]
 then
-  echo "`date -u` not changed"
+  echo "`date -u` ip地址未变化"
 else
   
   echo $IPADDRESS > ip.html
-  #/usr/local/bin/git add ip.html
-  #/usr/local/bin/git ci -m"ip change"
-  #/usr/local/bin/git push origin
+  $GITCOMMOND add ip.html
+  $GITCOMMOND  ci -m"ip change"
+  $GITCOMMOND  push origin
 fi
