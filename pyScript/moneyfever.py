f8139639bf4fe5651e0cea4f2bb21a14f3896303
oregami <oregami@163.com>
2017-04-20 14:43:28 +0800

    add finance

--- pyScript/moneyfever.py
+++ pyScript/moneyfever.py
@@ -5,7 +5,10 @@ import urllib2
 from uiautomator import Device
 import time
 import json
+import sys  
 
+reload(sys) 
+sys.setdefaultencoding('utf-8')
 
 def getback(d=Device()):
     d.press.back()
@@ -15,20 +18,16 @@ def getback(d=Device()):
     d.press.back()
     d.press.home()
 
-def moneyrun(account='', msg=''):
+def moneyrun(device=""):
     print "start...."
     d = Device('1ebf31e4')
-    xml = d.dump()
-    print xml
     time.sleep(2)
     while d.screen == "off":
         print "off"
         d.screen.on()
         time.sleep(2)
         d.swipe(200, 500, 200, 100, steps=10)
-        # d.swipe(1000,640,100,640,steps=10)
         time.sleep(2)
-        # print d.info
     print d.info
     # d.swipe(200, 500, 200, 100, steps=10)
     # d.press.home()
@@ -40,7 +39,7 @@ def moneyrun(account='', msg=''):
         time.sleep(1)
         # d.swipe(200, 800, 650, 800, steps=10)
         time.sleep(1)
-        print u'机京东不存在'
+        print u'手机京东不存在'
     d(text=u'手机京东').click()
     time.sleep(3)
     # d.swipe(200, 600, 550, 600, steps=100)
@@ -54,7 +53,71 @@ def moneyrun(account='', msg=''):
     # getback(d)
     return 1
 
-moneyrun()
+def jingdongFinance(device=""):
+    print "start...."
+    d = Device('1ebf31e4')
+    time.sleep(2)
+    while d.screen == "off":
+        print "off"
+        d.screen.on()
+        time.sleep(2)
+        d.swipe(200, 500, 200, 100, steps=10)
+        time.sleep(2)
+    print d.info
+    # d.swipe(200, 500, 200, 100, steps=10)
+    # d.press.home()
+    # time.sleep(1)
+    # d.swipe(200, 800, 650, 800, steps=10)
+    # time.sleep(1)
+    while not d(text=u'京东金融').exists:
+        d.press.home()
+        time.sleep(1)
+        # d.swipe(200, 800, 650, 800, steps=10)
+        time.sleep(1)
+        print u'京东金融不存在'
+    d(text=u'京东金融').click()
+    time.sleep(15)
+    print d.dump()
+    # d.swipe(200, 600, 550, 600, steps=100)
+    if d(text=u'立即登录体验').exists:
+        d(text=u'立即登录体验').click()
+        time.sleep(5)
+        print d.dump()
+
+    if d(text=u'用其他账号登录').exists:
+        d(text=u'用其他账号登录').click()
+        time.sleep(5)
+        print d.dump()
+    if d(text=u'手机京东一键登录').exists:
+        d(text=u'手机京东一键登录').click()
+        time.sleep(10)
+        print d.dump()
+        d.click(350, 800)
+    time.sleep(10)
+    print d.dump()
+    if d(text=u'白条可用额度').exists:
+        d(text=u'白条可用额度').click()
+        time.sleep(10)
+        print d.dump()
+    if d(text=u'1个提额包').exists:
+        d(text=u'1个提额包').click()
+        time.sleep(10)
+        print d.dump()
+    time.sleep(10)
+    d.click(600, 1200)
+    time.sleep(4)
+    print d.dump()
+
+    if d(description=u'领取').exists:
+        d(description=u'领取').click()
+        time.sleep(10)
+        print d.dump()
+    # getback(d)
+    return 1
+
+#moneyrun()
+
+jingdongFinance()
 # try:
 #     while True:
 #         moneyrun()
