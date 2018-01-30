745dc57604a4eac9fc4873af8dfe25863c485201
oregami <oregami@163.com>
2018-01-30 18:07:37 +0800

    use new url

--- shell/ipchange-git.sh
+++ shell/ipchange-git.sh
@@ -9,7 +9,7 @@ cd $GITREPOPATH
 $GITCOMMOND pull --rebase
 touch $GITREPOPATH/ip.html
 BEFORE=`cat ip.html`
-IPADDRESS=`curl -s 1212.ip138.com/ic.asp |  iconv -f gb2312 -t utf-8 |  grep -oe '\[\(.*\)\]' | sed 's/\[//g' | sed 's/\]//g'`
+IPADDRESS=`curl -s 2017.ip138.com/ic.asp |  iconv -f gb2312 -t utf-8 |  grep -oe '\[\(.*\)\]' | sed 's/\[//g' | sed 's/\]//g'`
 if [ "$IPADDRESS" == "$BEFORE" ]
 then
   echo "`date -u` ip地址未变化"
